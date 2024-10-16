954b57c2412c5e5fd977f47737fc4fa4dc19c461
AyushKrSingh000 <ayushkumarsingh0708@gmail.com>
2024-10-16 20:34:00 +0530

    added readme file

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('compound_db', 'root', 'ayush1084', {
+const sequelize = new Sequelize('compound_db', 'root', 'ayush1234', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306,
